ae37e1fa95374339dc891779dfa949dd4d01a6d5
Thummalaprasanth123 <thummalaprasanth8393@gmail.com>
2021-04-17 15:55:04 +0530

    My calac

---
+++ calc.js
@@ -0,0 +1,13 @@
+function AC()
+   {
+     document.getElementById("res").value="";
+   }
+  function show(input)
+  {
+    document.getElementById("res").value+=input;
+  } 
+  function calc()
+  {
+   var output=eval(document.getElementById("res").value);
+     document.getElementById("res").value=output;
+  }
